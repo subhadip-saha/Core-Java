94fbd7fda0e3b93eddb843a61ed3b2a878760004
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-03-03 08:20:20 +0530

    object initialization by using reference variable

---
+++ a3_object_initialization/Animal.java
@@ -0,0 +1,32 @@
+//object initialization by using methods
+
+package a3_object_initialization;	
+
+public class Animal {
+
+	String color;
+	int age;
+	
+	void initObj(String c, int a)
+	{
+		color = c;
+		age = a;
+	}
+	
+	void display()
+	{
+		System.out.println(color + " " + age);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Inside main method");
+		
+		Animal buzo = new Animal();	
+		buzo.initObj("GoldenBrown", 5); ;
+		buzo.display();
+				
+	}
+
+}
+
