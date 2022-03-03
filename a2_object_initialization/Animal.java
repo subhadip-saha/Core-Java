3bef5ec26efcf8b656269238a7f8e40dc09a26d9
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-03-03 08:20:01 +0530

    object initialization by using methods

---
+++ a2_object_initialization/Animal.java
@@ -0,0 +1,24 @@
+//object initialization by using reference variable
+
+package a2_object_initialization;	
+
+public class Animal {
+
+	String color;
+	int age;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Inside main method");
+		
+		Animal buzo = new Animal();	
+		buzo.color = "GoldenBrown";
+		buzo.age = 5;
+		
+		System.out.println(buzo.color + " " + buzo.age);
+		
+	}
+
+}
+
