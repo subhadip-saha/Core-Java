b2f001e38d5c94d9d6bc1d47e35a5063bb3c1473
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-04-06 21:42:35 +0530

    String Immutability | Why is String Object Immutable?

---
+++ SP117_to_SP133_String and Other Classes/sp119_string_immutability/StringImmutableDemo.java
@@ -0,0 +1,24 @@
+package sp119_string_immutability;
+
+public class StringImmutableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*String s = new String("Deepak");
+		System.out.println(s);				//Deepak
+		
+		s.concat(" Java");
+		System.out.println(s);				//Deepak
+		
+		s = s.concat(" Python");
+		System.out.println(s);				//Deepak Python
+		
+		String s1 = " Django";
+		s = s + s1;
+		System.out.println(s);			//Deepak Python Django */
+		
+
+	}
+
+}
